3a0745e358fd29d2c4c243be1b0820643920919e
selva <maildemotesting2001@gmail.com>
2025-06-25 13:45:20 +0530

    Initial commit

---
+++ src/main/java/mca/finalyearproject/smartDrive/SmartDrive/SmartDriveApplication.java
@@ -0,0 +1,13 @@
+package mca.finalyearproject.smartDrive.SmartDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mca/finalyearproject/smartDrive/SmartDrive/SmartDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package mca.finalyearproject.smartDrive.SmartDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
