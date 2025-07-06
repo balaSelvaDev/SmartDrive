798d8326da22e45bb5931beff5f765768ccd5a7b
selva <maildemotesting2001@gmail.com>
2025-07-06 17:17:54 +0530

    Multiple image upload

--- src/main/java/mca/finalyearproject/smartDrive/SmartDrive/Controller/ImageController.java
+++ src/main/java/mca/finalyearproject/smartDrive/SmartDrive/Controller/ImageController.java
@@ -3,11 +3,11 @@ package mca.finalyearproject.smartDrive.SmartDrive.Controller;
 import mca.finalyearproject.smartDrive.SmartDrive.ServiceImpl.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/images")
@@ -15,6 +15,7 @@ public class ImageController {
 
     @Autowired
     private CloudinaryService cloudinaryService;
+
 
     @PostMapping("/upload")
     public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
@@ -24,5 +25,16 @@ public class ImageController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Upload failed: " + e.getMessage());
         }
+    }
+
+    @PostMapping("/upload-multiple/{kycId}")
+    public ResponseEntity<List<String>> uploadKycImages(
+            @RequestParam("types") Integer userId,
+            @PathVariable("kycId") Integer kycId,
+            @RequestParam("files") MultipartFile[] files,
+            @RequestParam("types") String types
+    ) throws IOException {
+        List<String> urls = cloudinaryService.uploadImages(userId, kycId, files, types);
+        return ResponseEntity.ok(urls);
     }
 }
--- src/main/java/mca/finalyearproject/smartDrive/SmartDrive/ServiceImpl/CloudinaryService.java
+++ src/main/java/mca/finalyearproject/smartDrive/SmartDrive/ServiceImpl/CloudinaryService.java
@@ -2,21 +2,116 @@ package mca.finalyearproject.smartDrive.SmartDrive.ServiceImpl;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import mca.finalyearproject.smartDrive.SmartDrive.Entity.KycImageEntity;
+import mca.finalyearproject.smartDrive.SmartDrive.Entity.UserKycDetailsEntity;
+import mca.finalyearproject.smartDrive.SmartDrive.Repository.KycImageRepository;
+import mca.finalyearproject.smartDrive.SmartDrive.Repository.UserKycDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class CloudinaryService {
 
+    String USER_PROFILE_FOLDER_PATH = "Smart-drive-booking-hub/User profile image";
+    String PAN_CARD_PATH = "Smart-drive-booking-hub/Pan card";
+    String DRIVING_LICENSE_PATH = "Smart-drive-booking-hub/Driving license";
+    String AADHAR_CAR_PATH = "Smart-drive-booking-hub/Aadhar card";
+
     @Autowired
     private Cloudinary cloudinary;
 
+    @Autowired
+    private KycImageRepository kycImageRepository;
+
+    @Autowired
+    private UserKycDetailsRepository userKycDetailsRepository;
+
     public String uploadImage(MultipartFile file) throws IOException {
-        Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        Map<String, Object> options = ObjectUtils.asMap(
+                "folder", USER_PROFILE_FOLDER_PATH,
+                "public_id", "USER_" + 1
+        );
+        Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
         return uploadResult.get("secure_url").toString(); // Get URL of uploaded image
     }
+
+
+
+    public List<String> uploadImages(Integer userId, Integer kycId, MultipartFile[] files, String types) throws IOException {
+
+        UserKycDetailsEntity kyc = userKycDetailsRepository.findById(kycId)
+                .orElseThrow(() -> new RuntimeException("KYC ID not found"));
+
+        String[] typeList = types.split(",");
+        if (files.length != typeList.length) {
+            throw new IllegalArgumentException("Files count and types count must match");
+        }
+
+        List<String> uploadedUrls = new ArrayList<>();
+
+        for (int i = 0; i < files.length; i++) {
+            MultipartFile file = files[i];
+            String type = typeList[i].trim().toLowerCase();
+
+            long sizeInBytes = file.getSize();
+            double sizeInKB = sizeInBytes / 1024.0;
+            double sizeInMB = sizeInKB / 1024.0;
+            if (sizeInMB > 1.0) {
+                throw new IllegalArgumentException("File size exceeds 1MB limit");
+            }
+
+            String folder;
+            String publicId;
+
+            switch (type) {
+                case "profile":
+                    folder = "Smart-drive-booking-hub/User profile image";
+                    publicId = "USER_PROFILE_" + userId +"_" + kycId;
+                    break;
+                case "pancard":
+                    folder = "Smart-drive-booking-hub/Pan card";
+                    publicId = "USER_PANCARD_" + userId +"_" + kycId;
+                    break;
+                case "aadhar":
+                    folder = "Smart-drive-booking-hub/Aadhar card";
+                    publicId = "USER_AADHAR_" + userId +"_" + kycId;
+                    break;
+                case "driving_license":
+                    folder = "Smart-drive-booking-hub/Driving license";
+                    publicId = "USER_LICENSE_" + userId +"_" + kycId;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid document type: " + type);
+            }
+
+            Map<String, Object> options = ObjectUtils.asMap(
+                    "folder", folder,
+                    "public_id", publicId
+//                  ,"overwrite", true,
+//                   "resource_type", "auto"
+            );
+
+            String originalFilename = file.getOriginalFilename();
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
+            String uploadedUrl = (String) uploadResult.get("secure_url");
+            KycImageEntity image = new KycImageEntity();
+            image.setKyc(kyc);
+            image.setOriginalFileName(originalFilename);
+            image.setAlternateFileName((String) uploadResult.get("public_id"));
+            image.setImageUrl(uploadedUrl);
+            image.setStatus(true);
+
+            kycImageRepository.save(image);
+            uploadedUrls.add(uploadedUrl);
+        }
+
+        return uploadedUrls;
+    }
+
 }
