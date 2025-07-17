a37f6710722cd5940aa043fc8faefa6a79c061dc
selva <maildemotesting2001@gmail.com>
2025-07-17 15:28:28 +0530

    Microservice(centralized config and eureka server)

---
+++ src/main/java/mca/finalyearproject/smartDrive/SmartDrive/Controller/CentrailizedConfigMessage.java
@@ -0,0 +1,21 @@
+package mca.finalyearproject.smartDrive.SmartDrive.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@RequestMapping("/config")
+public class CentrailizedConfigMessage {
+
+//    @Value("${welcome.message}")
+//    public String msg;
+//
+//    @GetMapping
+//    public String message() {
+//        return msg;
+//    }
+
+
+}
