981af63a6ea0c34bb2a9346e1b9144af79c3ef63
selva <maildemotesting2001@gmail.com>
2025-10-08 12:23:22 +0530

    Minor changes

--- src/main/java/mca/finalyearproject/smartDrive/SmartDrive/Controller/LoginController.java
+++ src/main/java/mca/finalyearproject/smartDrive/SmartDrive/Controller/LoginController.java
@@ -27,7 +27,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/login")
-//@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
 
     @Autowired
@@ -45,18 +44,17 @@ public class LoginController {
     @Autowired
     private LoginCredentialRepository loginCredentialRepository;
 
+
     @Autowired
     private RoleRepository roleRepository;
 
     @PostMapping("/admin-login")
     public ResponseEntity<LoginCredentialEntity> loginForAdmin(@RequestBody LoginRequestDTO requestDTO) {
         try {
-//            System.out.println(requestDTO.toString());
             Optional<UserListEntity> userListEntity = userService.findByEmail(requestDTO.getEmailId());
             Optional<LoginCredentialEntity> loginCredentialEntity = loginCredentialRepository.findByUser(userListEntity.get());
             Role role = roleRepository.findById(loginCredentialEntity.get().getRole().getId())
                     .orElseThrow(() -> new RuntimeException("Role not found"));
-//            if (role.getName().equals(requestDTO.getAuthProvider())) {
             if (loginCredentialEntity.get().getAdminAccess()) {
                 authenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(requestDTO.getEmailId(), requestDTO.getPassword()));
                 String genereteJwtToken = jwtProvider.generateJwtToken(new UserPrincipal(userListEntity.get(), loginCredentialEntity.get()));
@@ -75,51 +73,17 @@ public class LoginController {
 
     @PostMapping
     public ResponseEntity<Boolean> createBrand(@RequestBody LoginRequestDTO requestDTO) {
-//        System.out.println(requestDTO);
-        System.out.println("<<<1>>>");
         authenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(requestDTO.getEmailId(), requestDTO.getPassword()));
-        System.out.println("<<<2>>>");
         Optional<UserListEntity> userListEntity = userService.findByEmail(requestDTO.getEmailId());
-        System.out.println("<<<3>>>");
         Optional<LoginCredentialEntity> loginCredentialEntity = loginCredentialRepository.findByUser(userListEntity.get());
-        System.out.println("<<<4>>>");
         String genereteJwtToken = jwtProvider.generateJwtToken(new UserPrincipal(userListEntity.get(), loginCredentialEntity.get()));
-        System.out.println("<<<5>>>");
         HttpHeaders httpHeaders = new HttpHeaders();
-        System.out.println("<<<6>>>");
         httpHeaders.add(SecurityConstant.HEADER_NAME, genereteJwtToken);
-        System.out.println("<<<7>>>");
         return new ResponseEntity<>(true, httpHeaders, HttpStatus.OK);
-//         return loginService.loginCheck(requestDTO);
     }
-
-    // as of now, i have planned to not used
-    @PostMapping("/verification-code")
-    public Boolean checkVerificationCode(@RequestBody LoginVerificationCodeRequestDTO requestDTO) {
-        return loginService.checkVerificationCode1(requestDTO);
-    }
-
-    // as of now, i have planned to not used
-    @PostMapping("/reset-verification-code")
-    public LoginResponseDTO resetVerificationCode(@RequestBody ResetDetailsResponseDTO requestDTO) {
-        return loginService.resetVerificationCode(requestDTO);
-    }
-
-//    @GetMapping("/login/google")
-//    public ResponseEntity<String > loginGoogleAuth(HttpServletResponse response) throws IOException {
-//        response.sendRedirect("/oauth2/authorization/google");
-//        return ResponseEntity.ok("Redirecting ..");
-//    }
-//    @GetMapping("/loginSuccess")
-//    public ResponseEntity<? > handleGoogleSuccess(OAuth2AuthenticationToken oAuth2AuthenticationToken){
-//        User user = userService.loginRegisterByGoogleOAuth2(oAuth2AuthenticationToken);
-//        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create("http://localhost:3000/home")).build();
-//    }
 
     @PostMapping("/google/generate-jwt-token")
     public ResponseEntity<?> generate(@RequestBody LoginRequestDTO requestDTO) {
-//        authenticationProvider
-//                .authenticate(new UsernamePasswordAuthenticationToken(requestDTO.getEmailId(), requestDTO.getPassword()));
         UserListEntity userListEntity = userService.findByEmail(requestDTO.getEmailId()).orElseThrow(() -> new RuntimeException("UserListEntity not found for user"));
         LoginCredentialEntity loginCredentialEntity = loginCredentialRepository.findByUser(userListEntity).orElseThrow(() -> new RuntimeException("LoginCredentialEntity not found for user"));
         String genereteJwtToken = jwtProvider.generateJwtToken(new UserPrincipal(userListEntity, loginCredentialEntity));
@@ -128,7 +92,6 @@ public class LoginController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(SecurityConstant.HEADER_NAME, genereteJwtToken);
         return new ResponseEntity<>("Success", httpHeaders, HttpStatus.OK);
-        // return loginService.loginCheck(requestDTO);
     }
 
 }
